2ef07a52f1436f96fc4ca2dd64afae6c2fe1e442
fadialsalti <eng_fadi77@hotmail.com>
2024-03-12 19:42:18 +0100

    add config for mysql and mongo

--- create.sh
+++ create.sh
@@ -1,13 +1,16 @@
 #/bin/bash
 
-lsof -i :80 | grep LISTEN | awk '{print $2}' | xargs kill -9
+sudo lsof -i :80 | grep LISTEN | awk '{print $2}' | xargs kill -9
 
 kind create cluster --config cluster-config.yml 
+
+kubectl apply --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 
 echo "172.19.0.2      rabbitmq-manager.com" >> /etc/hosts
 echo "127.0.0.1       mp3converter.com" >> /etc/hosts
 
 cd python/src/auth/
+kubectl create configmap mysql-init-script --from-file=init.sql
 kubectl apply -f manifests/
 
 cd ../rabbit/
@@ -16,5 +19,9 @@ kubectl apply -f manifests/
 cd ../gateway/
 kubectl apply -f manifests/
 
-cd ../coverter/
+cd ../converter/
 kubectl apply -f manifests/
+
+
+# curl -X POST http://mp3converter.com/login -u georgio@email.com:Admin123 -i  -v
+# curl -X POST -F 'file=@./videoplayback.mp4' -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6Imdlb3JnaW9AZW1haWwuY29tIiwiZXhwIjoxNzEwMzU0NjgwLCJpYXQiOjE3MTAyNjgyODAsImFkbWluIjp0cnVlfQ.WZIsOsyePF441jvKNSyXFVIOG-EjfdJD-n2oWHCDoYI' http://mp3coverter.com/upload
--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -8,9 +8,9 @@ from bson.objectid import ObjectId
 
 server = Flask(__name__)
 
-mongo_video = PyMongo(server, uri="mongodb://host.minikube.internal:27017/videos")
+mongo_video = PyMongo(server, uri="mongodb://mongodb:27017/videos")
 
-mongo_mp3 = PyMongo(server, uri="mongodb://host.minikube.internal:27017/mp3s")
+mongo_mp3 = PyMongo(server, uri="mongodb://mongodb:27017/mp3s")
 
 fs_videos = gridfs.GridFS(mongo_video.db)
 fs_mp3s = gridfs.GridFS(mongo_mp3.db)
